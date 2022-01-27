d8ddaa1e3cb735e23224bf9da25af77986150b6a
sksn <ps.net26@gmail.com>
2022-01-27 21:45:00 +0900

    add create pair

--- random_pair.rb
+++ random_pair.rb
@@ -6,14 +6,40 @@ class RandomPair
 
   def input_member
     @number.times do 
+      print '> '
       @members << gets.chomp
     end
+  end
+
+  def shuffle
+    @members.shuffle!
+  end
+
+  def pick_pair
+    puts "\e[H\e[2J" # console clear
+    while @members.size > 0
+      @members.size == 3 ? trio : pair
+      sleep(2)
+    end
+  end
+
+  private
+  def pair
+    p @members.shift(2).join(' <=> ')
+  end
+
+  def trio
+    @members << @members.first
+    p @members.shift(4).join(' => ')
   end
 end
 
 print 'please input number of people => '
 
-shuffle_pair = RandomPair.new(gets)
+random_pair = RandomPair.new(gets)
 
-puts 'please input member'
-shuffle_pair.input_member
+puts 'please input member name '
+random_pair.input_member
+
+random_pair.shuffle
+random_pair.pick_pair
